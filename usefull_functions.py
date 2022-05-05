fa20b90e1b830622732e58372f549eea78dc7e45
Alexander <48926283+canek13@users.noreply.github.com>
2022-05-05 12:35:23 +0300

    reduce_memory usage datetime type
    bypassing of datetime type added in reduce_memory_usage function

--- usefull_functions.py
+++ usefull_functions.py
@@ -122,7 +122,8 @@ def reduce_memory_usage(df: pd.DataFrame,
                     df[col] = df[col].astype(np.int32)
                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
                     df[col] = df[col].astype(np.int64)  
-            else:
+            elif not ( col_type == np.dtype('datetime64[ns]') \
+                    or col_type == np.dtype('<M8[ns]') ): # or col_type == np.dtype('>M8[ns]')
                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
                     df[col] = df[col].astype(np.float16)
                 elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
